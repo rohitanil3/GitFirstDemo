0e8acdaa4c1032da2cb31d3dd64196c24dc9d12a
RK <offen.3@gmail.com>
2024-07-06 20:54:13 +0530

    Change made by Rohit in controller file

--- src/main/java/com/example/gitfirstdemo/controller/GitController.java
+++ src/main/java/com/example/gitfirstdemo/controller/GitController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/git")
 public class GitController {
-
+    @GetMapping("/name")
+    public String getname(){
+        return "Hi Ram..";
+    }
     @GetMapping("/hi")
     public String gettingHello(){
         return "Hi Rohit....";
